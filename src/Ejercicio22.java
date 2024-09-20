1a8034ad2372d4b77fabf3dda87625858c1e34a0
aargarjua <aargarjua@alu.edu.gva.es>
2024-09-20 11:43:07 +0200

    Ejercicio22

---
+++ src/Ejercicio22.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ejercicio22 {
+    public static void main(String[] args) {
+
+        int num;
+        int positivos = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++) {
+
+            num = sc.nextInt();
+            if (num >= 0) {
+                positivos ++;
+
+
+            }
+
+        }
+
+        System.out.println("Positivos: " + positivos);
+
+    }
+}
